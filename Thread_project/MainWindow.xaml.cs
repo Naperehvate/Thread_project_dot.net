6610fcd1a19c1350bc1354266666413f10869662
tropikano647 <tropikano647@mail.ru>
2023-07-25 00:25:32 +0500

    Поменять abort

--- Thread_project/MainWindow.xaml.cs
+++ Thread_project/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ namespace Thread_project
         private int min;
         private int max;
         private Thread? _thread;
+        private Thread? _thread_fibo;
         public MainWindow()
         {
             InitializeComponent();
@@ -17,10 +18,33 @@ namespace Thread_project
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            if (But_Start.Content.ToString() == "Стоп")
+            {
+                if(_thread!=null && _thread.IsAlive)
+                {
+                    _thread.Abort();
+                }
+                But_Start.Content = "Старт";
+            }
             min = Convert.ToInt32(TextBox_Min.Text);
             max = Convert.ToInt32(TextBox_Max.Text);
             _thread = new Thread(GeneratedNumber);
             _thread.Start();
+            But_Start.Content = "Стоп";
+        }
+
+        private void Button_Click_Fibo(object sender, RoutedEventArgs e)
+        {
+            if (But_Start_Fibo.Content.ToString() == "Стоп")
+            {
+                if (_thread_fibo != null && _thread_fibo.IsAlive)
+                {
+                    _thread_fibo.Abort();
+                    But_Start_Fibo.Content = "Старт";
+                }
+            }
+            _thread_fibo = new Thread(GeneratedFibonachi);
+            _thread_fibo.Start();
         }
 
         private void GeneratedNumber()
@@ -31,9 +55,31 @@ namespace Thread_project
                 {
                     this.Dispatcher.Invoke(() =>
                     {
+                        
                         TextBox_Result.Text += " " + i.ToString();
                     });
+                    Thread.Sleep(200);
                 }
+
+            }
+        }
+
+        private void GeneratedFibonachi()
+        {
+            int a = 0;
+            int b = 1;
+
+            while (true)
+            {
+                int c = a + b;
+                a = b;
+                b = c;
+                this.Dispatcher.Invoke(() =>
+                {
+                    TextBox_Result_Fibo.Text += " " + a.ToString();
+                });
+                
+                Thread.Sleep(200);
             }
         }
 
@@ -48,7 +94,7 @@ namespace Thread_project
             {
                 if (number % i == 0 || number % (i + 2) == 0) return false;
             }
-            //uhuygfyuoiguigu
+            
             return true;
         }
     }
