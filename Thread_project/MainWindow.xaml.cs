73689bd4bf38e597deeea768c46317ef22a56813
tropikano647 <tropikano647@mail.ru>
2023-07-24 21:43:32 +0500

    Добавил

--- Thread_project/MainWindow.xaml.cs
+++ Thread_project/MainWindow.xaml.cs
@@ -1,13 +1,20 @@
-﻿using System.Windows;
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Threading;
+using System.Windows;
 
 namespace Thread_project
 {
     public partial class MainWindow : Window
     {
+        private int min;
+        private int max;
+        private Thread? _thread;
         public MainWindow()
         {
             InitializeComponent();
         }
 
     }
+
 }
