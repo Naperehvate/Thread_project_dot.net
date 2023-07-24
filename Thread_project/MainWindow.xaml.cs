19513b35d8d7d93dc34fe590557f2f7014273254
User <User@COMPUTER>
2023-07-24 17:46:57 +0500

    Добавьте файлы проекта.

---
+++ Thread_project/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Thread_project
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
