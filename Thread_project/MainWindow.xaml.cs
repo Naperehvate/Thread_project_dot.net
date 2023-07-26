ad812eb266ddc23db741dd8a9b76a1730a5b4872
Naperehvate <tropikano647@mail.ru>
2023-07-26 18:58:59 +0500

    Revert "Переработан код на правильную остановку"
    This reverts commit 2d14484a7bc942e3d4531998c30634d7c3236e29.

--- Thread_project/MainWindow.xaml.cs
+++ Thread_project/MainWindow.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Thread_project
@@ -9,88 +9,76 @@ namespace Thread_project
     {
         private int min;
         private int max;
-        private CancellationTokenSource cancellationTokenSource;
-        private CancellationToken cancellationToken;
-
+        private Thread? _thread;
+        private Thread? _thread_fibo;
         public MainWindow()
         {
             InitializeComponent();
-            cancellationTokenSource = new CancellationTokenSource();
-            cancellationToken = cancellationTokenSource.Token;
-
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
             if (But_Start.Content.ToString() == "Стоп")
             {
-                cancellationTokenSource.Cancel();
+                if(_thread!=null && _thread.IsAlive)
+                {
+                    _thread.Abort();
+                }
                 But_Start.Content = "Старт";
             }
-            else
-            {
-                TextBox_Result.Text = string.Empty;
-                min = Convert.ToInt32(TextBox_Min.Text);
-                max = Convert.ToInt32(TextBox_Max.Text);
-                cancellationTokenSource = new CancellationTokenSource();
-                cancellationToken = cancellationTokenSource.Token;
-                But_Start.Content = "Стоп";
-
-                await Task.Run(() => GeneratedNumber(cancellationToken));
-            }
+            min = Convert.ToInt32(TextBox_Min.Text);
+            max = Convert.ToInt32(TextBox_Max.Text);
+            _thread = new Thread(GeneratedNumber);
+            _thread.Start();
+            But_Start.Content = "Стоп";
         }
 
-        private async void Button_Click_Fibo(object sender, RoutedEventArgs e)
+        private void Button_Click_Fibo(object sender, RoutedEventArgs e)
         {
             if (But_Start_Fibo.Content.ToString() == "Стоп")
             {
-                cancellationTokenSource.Cancel();
-                But_Start_Fibo.Content = "Старт";
+                if (_thread_fibo != null && _thread_fibo.IsAlive)
+                {
+                    _thread_fibo.Abort();
+                    But_Start_Fibo.Content = "Старт";
+                }
             }
-            else
-            {
-                TextBox_Result_Fibo.Text = string.Empty;
-                min = Convert.ToInt32(TextBox_Min.Text);
-                max = Convert.ToInt32(TextBox_Max.Text);
-                cancellationTokenSource = new CancellationTokenSource();
-                cancellationToken = cancellationTokenSource.Token;
-                But_Start_Fibo.Content = "Стоп";
-
-                await Task.Run(() => GeneratedFibonachi(cancellationToken));
-            }
+            _thread_fibo = new Thread(GeneratedFibonachi);
+            _thread_fibo.Start();
         }
 
-        private void GeneratedNumber(CancellationToken cancellationToken)
+        private void GeneratedNumber()
         {
             for (int i = min; i <= max; i++)
             {
-                if (IsPrime(i) && !cancellationToken.IsCancellationRequested)
+                if (IsPrime(i))
                 {
                     this.Dispatcher.Invoke(() =>
                     {
+                        
                         TextBox_Result.Text += " " + i.ToString();
                     });
                     Thread.Sleep(200);
                 }
+
             }
         }
 
-        private void GeneratedFibonachi(CancellationToken cancellationToken)
+        private void GeneratedFibonachi()
         {
             int a = 0;
             int b = 1;
-            int c = 0;
 
-            while (!cancellationToken.IsCancellationRequested && b <= max)
+            while (true)
             {
-                c = a + b;
+                int c = a + b;
                 a = b;
                 b = c;
                 this.Dispatcher.Invoke(() =>
                 {
-                    TextBox_Result_Fibo.Text += " " + c.ToString();
+                    TextBox_Result_Fibo.Text += " " + a.ToString();
                 });
-
+                
                 Thread.Sleep(200);
             }
         }
@@ -106,8 +94,9 @@ namespace Thread_project
             {
                 if (number % i == 0 || number % (i + 2) == 0) return false;
             }
-
+            
             return true;
         }
     }
+
 }
